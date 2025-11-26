9d9a298a0dba81f4633fe2a16c6367b5ea6a78ac
moh123adan <maxamedadan169@gmail.com>
2025-11-26 18:24:33 +0300

    Revamp Contact page to enhance user engagement. Updated the contact form with new fields for user inquiries and replaced the office address section with a more inviting message. Adjusted CSS for improved form layout and responsiveness.

--- pages/contact.js
+++ pages/contact.js
@@ -10,36 +10,50 @@ export default function Contact() {
                             <div className="row align-items-center">
                                 <div className="col-lg-6">
                                     <div className="inner-contact-img">
-                                        <img src="/assets/img/images/contact_img.jpg" alt="" />
+                                        <img src="/assets/img/ogalsan/contact.jpg" alt="Contact OgaalSan Consultancy" />
                                     </div>
                                 </div>
                                 <div className="col-lg-6">
                                     <div className="inner-contact-info">
-                                        <h2 className="title">Our Office Address</h2>
-                                        <div className="contact-info-item">
-                                            <h5 className="title-two">USA Office</h5>
-                                            <ul className="list-wrap">
-                                                <li>100 Wilshire Blvd, Suite 700 Santa <br /> Monica, CA 90401, USA</li>
-                                                <li>+1 (310) 620-8565</li>
-                                                <li>info@gmail.com</li>
-                                            </ul>
-                                        </div>
-                                        <div className="contact-info-item">
-                                            <h5 className="title-two">Australia Office</h5>
-                                            <ul className="list-wrap">
-                                                <li>100 Wilshire Blvd, Suite 700 Santa <br /> Monica, CA 90401, USA</li>
-                                                <li>+1 (310) 620-8565</li>
-                                                <li>info@gmail.com</li>
-                                            </ul>
+                                        <h2 className="title">Get in Touch With OgaalSan</h2>
+                                        <p>Share your ICT, training, or digital innovation needs and our team will get back to you as soon as possible.</p>
+                                        <div className="contact-form contact-form-inner">
+                                            <form action="#">
+                                                <div className="row">
+                                                    <div className="col-md-6">
+                                                        <div className="form-grp">
+                                                            <input type="text" placeholder="Name *" />
+                                                        </div>
+                                                    </div>
+                                                    <div className="col-md-6">
+                                                        <div className="form-grp">
+                                                            <input type="email" placeholder="E-mail *" />
+                                                        </div>
+                                                    </div>
+                                                    <div className="col-md-6">
+                                                        <div className="form-grp">
+                                                            <input type="text" placeholder="Phone / WhatsApp *" />
+                                                        </div>
+                                                    </div>
+                                                    <div className="col-md-6">
+                                                        <div className="form-grp">
+                                                            <input type="text" placeholder="Subject *" />
+                                                        </div>
+                                                    </div>
+                                                    <div className="col-md-12">
+                                                        <div className="form-grp">
+                                                            <textarea placeholder="How can we help you? *" />
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <button type="submit" className="btn btn-three w-100">Send Message</button>
+                                            </form>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </section>
-                    <div className="contact-map">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3152.332792000835!2d144.96011341744386!3d-37.805673299999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad65d4c2b349649%3A0xb6899234e561db11!2sEnvato!5e0!3m2!1sen!2sbd!4v1685027435635!5m2!1sen!2sbd" allowFullScreen loading="lazy" />
-                    </div>
                 </div>
             </Layout>
         </>
